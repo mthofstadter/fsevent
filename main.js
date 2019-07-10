ae688e5db858db0f553b330530ea8df59b117576
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-10 16:29:17 -0500

    css time
    Lots and lots of stuff. Based on px rather than browser size. But looking pretttier

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("here");
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
+
+function init() {
+}
+
+function sarah() {
+  var name = document.getElementById("first-name").value;
+  console.log(name);
+  if(name == "sarah") {
+    window.alert("bussy");
+  }
+  console.log("running");
+}
