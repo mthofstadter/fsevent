ca3125826059008264082561e22f99b0d264a0ba
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-15 14:10:02 -0500

    maximillian width
    image fix (?) maybe

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
 
-/*
 function init() {
   mobileCheck()
 }
