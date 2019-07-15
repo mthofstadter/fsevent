db92c93ef54f02d9e3a9f23ab1212c44e043031a
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-07-15 13:02:09 -0500

    mobile check mate
    removed JS mobile check

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init();
 });
 
+/*
 function init() {
   mobileCheck()
 }
@@ -11,6 +12,7 @@ function mobileCheck() {
       document.location = "thanks.html";
   }
 }
+*/
 
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
